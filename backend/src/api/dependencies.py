aef7c09bdf79314a0ccdc464c3007f0634568b13
Igor39-dev <19gorokhov90@gmail.com>
2026-06-09 23:42:12 +0200

    add DI for sessions
     transfer health_check on DBDep

---
+++ backend/src/api/dependencies.py
@@ -0,0 +1,15 @@
+from collections.abc import AsyncGenerator
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.src.database import session as session_factory
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with session_factory() as db_session:
+        yield db_session
+
+
+DBDep = Annotated[AsyncSession, Depends(get_db_session)]
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI, HTTPException
 import uvicorn
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import text
 
-from backend.src.database import check_db_connection
+from backend.src.api.dependencies import DBDep
 
 
 app = FastAPI(
     title="Management System",
-    description="API для управления бизнесном"
+    description="API для управления бизнесном",
 )
 
-@app.get("/health", tags=["Check DB connection"])
-async def health_check():
 
-    db_ok = await check_db_connection()
-    if not db_ok:
-        raise HTTPException(status_code=500, detail="Database connection failed")
+@app.get("/health", tags=["Check DB connection"])
+async def health_check(db: DBDep):
+    try:
+        await db.execute(text("SELECT 1"))
+    except Exception:
+        raise HTTPException(status_code=500, detail="Нет подлючения к базе данных")
     return {"status": "ok"}
 
 
